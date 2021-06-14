824cd846fa085178a46f811cb86dd662df3064c2
nienkevonk <84348574+nienkevonk@users.noreply.github.com>
2021-06-14 22:31:37 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const makePizza = function () {
+    console.log('make the dough');
+    console.log('rest the dough');
+    console.log('preheat the oven');
+    console.log('cut the ingredients');
+    console.log('roll out the dough');
+    console.log('put tomato sauce on top of the dough');
+    console.log('put the ingredients on top of the tomato sauce');
+    console.log('put a lot of cheese on top of that');
+    console.log('put the pizza into the oven');
+    console.log('serve the pizza');
+};
+
+makePizza();
+makePizza();
+
+const makeSushi = function () {
+    console.log('get some rice');
+    console.log('get some sashimi sticks');
+    console.log('get some nori');
+    console.log('get some cucumber');
+    console.log('roll the sushi with all the ingredients');
+    console.log('cut the roll into little pieces');
+    console.log('serve the sushi');
+};
+
+makeSushi();
+makeSushi();
+makeSushi();
